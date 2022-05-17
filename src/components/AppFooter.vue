ea97ed725750abb2f7df671a6719274d1b7e4f12
Cix8 <francesco.natal_fn@libero.it>
2022-05-17 19:12:19 +0200

    add contents in AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -44,8 +44,39 @@
           <li><a href="">Link</a></li>
         </ul>
       </div>
+      <div class="img-container">
+        <img src="../assets/img/dc-logo-bg.png" />
+      </div>
     </div>
-    <div class="bottom"></div>
+    <div class="bottom">
+      <div class="left">
+        <a href="">SIGN-UP NOW!</a>
+      </div>
+      <div class="right">
+        <div class="anchor-container">
+          <a href="">FOLLOW US</a>
+        </div>
+        <div class="icons-container">
+          <ul>
+            <li>
+              <img src="../assets/img/footer-facebook.png" />
+            </li>
+            <li>
+              <img src="../assets/img/footer-twitter.png" />
+            </li>
+            <li>
+              <img src="../assets/img/footer-youtube.png" />
+            </li>
+            <li>
+              <img src="../assets/img/footer-pinterest.png" />
+            </li>
+            <li>
+              <img src="../assets/img/footer-periscope.png" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -58,15 +89,21 @@ export default {
 <style lang="scss" scoped>
 footer {
   background-image: url("../assets/img/footer-bg.jpg");
-  padding: 0 150px;
+
+  .top,
+  .bottom {
+    padding: 30px 150px;
+  }
 
   .top {
     display: flex;
-    padding: 30px 0;
+    position: relative;
     color: #747779;
 
     .col {
-      margin-left: 50px;
+      &:not(.col:first-child) {
+        margin-left: 50px;
+      }
 
       &:first-child {
         ul:first-child {
@@ -78,6 +115,63 @@ footer {
         color: #ffffff;
         font-weight: 700;
         margin-bottom: 5px;
+      }
+    }
+
+    .img-container {
+      position: absolute;
+      width: 400px;
+      top: 50%;
+      right: 20%;
+      transform: translate(0, -50%);
+    }
+  }
+
+  .bottom {
+    position: relative;
+    z-index: 99;
+    display: flex;
+    align-items: center;
+    background-color: #303030;
+
+    .left,
+    .right {
+      width: 50%;
+    }
+
+    .left {
+      color: #ffffff;
+
+      a {
+        display: inline-block;
+        padding: 10px;
+        border: 2px solid #0282f9;
+        font-weight: 700;
+      }
+    }
+
+    .right {
+      display: flex;
+      align-items: center;
+
+      .anchor-container {
+        padding: 10px 0;
+        font-size: 1.2rem;
+        font-weight: 900;
+        color: #0282f9;
+      }
+
+      .icons-container {
+        flex-grow: 1;
+
+        ul {
+          display: flex;
+          padding: 0 20px;
+
+          li {
+            margin: 0 10px;
+          }
+        }
       }
     }
   }
